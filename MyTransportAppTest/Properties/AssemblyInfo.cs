eb96e44f482e5b46e23fdfc9ce3e3205e90c095f
eweennx <Evan_Mueller@sluz.ch>
2021-04-16 12:43:55 +0200

    Unite Tests Korrekt und eigener Unit Test

---
+++ MyTransportAppTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MyTransportAppTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("MyTransportAppTest")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1ca5ff36-53c4-4557-bfa7-eb0055aac308")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ MyTransportAppTest/UnitTest1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Windows.Forms;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyTransportApp;
+
+namespace MyTransportAppTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            TextBox textboxTest = new TextBox();
+            textboxTest.Text = "Rothenbur";
+
+            ListBox listboxTest = new ListBox();
+
+            Vorschlaege.VorschlaegeFunction(textboxTest.Text, listboxTest);
+
+            Assert.AreEqual("Rothenburg", listboxTest.Items[0].ToString());
+        }
+    }
+}
